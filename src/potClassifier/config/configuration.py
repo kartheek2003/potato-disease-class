72e5dd70da9273d04982d5f55ed8ba5636b4cdee
kartheek <kartheekakkabathula@gmail.com>
2025-02-20 21:53:10 +0530

    callbacks

--- src/potClassifier/config/configuration.py
+++ src/potClassifier/config/configuration.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from potClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from potClassifier.utils.common import read_yaml,create_directories
 from potClassifier.entity.config_entity import dataingestionconfig
-from potClassifier.entity.config_entity import PrepareBaseModelConfig
+from potClassifier.entity.config_entity import PrepareBaseModelConfig,PrepareCallbacksConfig
 from potClassifier.constants import *
 
-
+import os
 
 
 
@@ -45,3 +45,15 @@ class ConfigurationManager:
                                                            params_include_top=self.params.INCLUDE_TOP,
                                                            )
         return preparebasemodelconfig
+    
+    def get_prepare_callbacks_config(self)->PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([Path(model_ckpt_dir),Path(config.tensorboard_root_log_dir)])
+        
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir= Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+        return prepare_callbacks_config
